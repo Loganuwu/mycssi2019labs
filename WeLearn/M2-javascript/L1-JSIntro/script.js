da1e8d79afbbd45e725e76a294f16257e8993c4a
Logan Barajas <baraj089@csusm.edu>
2019-07-24 12:01:46 -0700

    JavaScript day1

---
+++ WeLearn/M2-javascript/L1-JSIntro/script.js
@@ -0,0 +1,12 @@
+console.log("script running!");
+
+let name = ("Charlie");
+console.log("Hello " + name + "!");
+let age = 16;
+console.log("You are " + age + " Years old!");
+
+if (age >= 16){
+  console.log("You can Drive!");
+} else {
+  console.log("You can not drive yet.");
+}
